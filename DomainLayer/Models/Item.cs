a9448303fdc59a6feaefa48cfb3aecad5294016f
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-05-01 16:10:39 +0200

    I did the Item ,Supplier, stock, User and LoginModels

---
+++ DomainLayer/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public partial class Item
+    {
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; } = null!;
+
+        public string? Category { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+
+        public virtual ICollection<Stock> Stocks { get; set; } //= new List<Stock>();
+    }
+
+}
---
+++ DomainLayer/Models/Stock.cs
@@ -0,0 +1,21 @@
+﻿namespace DomainLayer.Models
+{
+    public partial class Stock
+    {
+        public int StockId { get; set; }
+
+        public int ItemId { get; set; }
+
+        public int SupplierId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public DateTime? ReceivedDate { get; set; }
+
+        public virtual Item Item { get; set; } = null!;
+
+        public virtual Supplier Supplier { get; set; } = null!;
+    }
+}
