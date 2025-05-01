e732a05e9bc6f529513c22716ffe41d4c04a67c5
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-05-01 19:01:15 +0200

    I added relationbetween API and Application Layer & I prepared DTOs Files

---
+++ Application/ACommen/DTOs/StockDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationLayer.ACommen.DTOs
+{
+    public class StockDTO
+    {
+        public int StockId { get; set; }
+        public int ItemId { get; set; }
+        public int SupplierId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public DateTime ReceivedDate { get; set; }
+    }
+}
